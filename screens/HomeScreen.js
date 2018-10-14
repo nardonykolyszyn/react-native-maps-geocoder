f65b7cf095674c2e1a6aeeb3930cae3e925f7be8
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2018-10-14 13:53:49 -0500

    geocode locations by name

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,126 +1,79 @@
 import React from 'react';
 import {
-  Image,
-  Platform,
-  ScrollView,
   StyleSheet,
-  Text,
-  TouchableOpacity,
   View,
-  Button
 } from 'react-native';
-import { WebBrowser } from 'expo';
+import { WebBrowser, MapView } from 'expo';
+import styled from 'styled-components';
+import Geocoder from 'react-native-geocoding';
 
-import { MonoText } from '../components/StyledText';
+Geocoder.init('AIzaSyCR4uEFeksRL8oqgg9dXM7npxdsPArJQak')
+
+const SearchInput = styled.TextInput`
+  color: black;
+  padding: 10px;
+  border: 2px solid palevioletred;
+  margin: 10px;
+  width: 300px;
+  height: 40px;
+  border-radius: 3px;
+  background-color: papayawhip;
+  box-shadow: 0 6px 6px rgba(0,0,0,0.23);
+`
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
+  
+  state = {
+    coordinates: {
+      latitude: 4.7306779,
+      longitude: -74.0358762,
+      latitudeDelta: 0.00922,
+      longitudeDelta: 0.00921
+    },
+    address: ''
+  }
 
   handleLearnMorePress = () => {
     WebBrowser.openBrowserAsync('http://google.com');
   };
 
+  searchForLocation = async () => {
+    Geocoder.from(this.state.address)
+      .then(json => {
+        var location = json.results[0].geometry.location;
+        this.setState({
+          coordinates: {
+            latitude: location.lat,
+            longitude: location.lng,
+            latitudeDelta: 0.00922,
+            longitudeDelta: 0.00921
+          }
+        })
+
+      })
+      .catch(error => console.log(error));
+  }
+
   render() {
     return (
-      <View style={styles.tabBarInfoContainer}>
-        <Button
-          onPress={() => this.handleLearnMorePress()}
-          title="Browser"
-          color="blue"
-          accessibilityLabel="Learn more about this purple button"
+      <MapView
+        style={{flex: 1}}
+        region={this.state.coordinates}
+      >
+      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+        <SearchInput
+          placeholder={'Where would you like to go?'}
+          onBlur={() => this.searchForLocation('Parque 93')}
+          onChangeText={(address) => this.setState({address: address})}
         />
       </View>
+      </MapView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
 });
